40355d1f2cdc498a09cc26901f4cfa054f19fde3
skydanc <75074028+skydanc@users.noreply.github.com>
2023-02-05 18:07:46 +0000

    Adding code for OpenAI API

--- questionAnswerCli.py
+++ questionAnswerCli.py
@@ -1,36 +1,28 @@
 #!/usr/bin/env python3
 
-"""
-An openai api key is required to use this script.
-This uses an advanced GPT-3 model and I also used AI via Github Copilot to write this command-line interface.
-
-
-
 import os
 import openai
 import click
 
 def submit_question(text):
-    """Submit a question to OpenAI API"""
     openai.api_key = os.getenv("OPENAI_API_KEY")
     prompt = text
     result = openai.Completion.create(
+        engine="text-davinci-002",
         prompt=prompt,
-        temperature=0,
         max_tokens=300,
-        top_p=1,
-        frequency_penalty=0,
-        presence_penalty=0,
-        model="text-davinci-002"
-    )["choices"][0]["text"].strip(" \n")
-    return result
+        n=1,
+        stop=None,
+        temperature=0.5,
+    )
+    answer = result["choices"][0]["text"].strip()
+    return answer
 
 @click.command()
 @click.argument("text")
 def main(text):
-    """Ask the OpenAI API a question to get an answer"""
-    print(submit_question(text))
+    answer = submit_question(text)
+    print(answer)
 
 if __name__ == "__main__":
     main()
-
